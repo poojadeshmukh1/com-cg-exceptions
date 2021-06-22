1412f9c150d1b435fb094df12771acbf41535fa7
Pooja Satish Deshmukh <83465631+poojadeshmukh1@users.noreply.github.com>
2021-06-22 11:55:17 +0530

    Add files via upload

---
+++ FileNotFoundDemo.java
@@ -0,0 +1,22 @@
+package com.cg.exception;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileNotFoundDemo {
+	
+	    public static void main(String args[])  {
+	        try {
+	 
+	            // Following file does not exist
+	            File file = new File("E://file.txt");
+	 
+	            FileReader fr = new FileReader(file);
+	        } catch (FileNotFoundException e) {
+	           System.out.println("File does not exist");
+	        }
+	    }
+	}
+
+
---
+++ InvlidUser.java
@@ -0,0 +1,10 @@
+package com.cg.exception;
+
+public class InvlidUser extends Exception{
+	
+
+		   public InvlidUser(String s) {
+			super (s);
+		}
+
+}
